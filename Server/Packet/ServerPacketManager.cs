c493a4dd5f42c394666748c4884d1c6c114ce269
DeskTop_LHJ <kistudentmeta5@gmail.com>
2024-11-07 22:42:09 +0900

    Feat : CommitTestLine

--- Server/Packet/ServerPacketManager.cs
+++ Server/Packet/ServerPacketManager.cs
@@ -14,7 +14,7 @@ public class PacketManager
     Dictionary<ushort, Action<PacketSession, ArraySegment<byte>>> _onRecv = new();
     Dictionary<ushort, Action<PacketSession, IPacket>> _handler = new();
 
-
+    //CommitTest
     public void Register()
     {
        _onRecv.Add((ushort)PacketID.C_PlayerInfoReq, MakePacket<C_PlayerInfoReq>);
